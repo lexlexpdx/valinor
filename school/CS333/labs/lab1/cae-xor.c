04fd199073202a0b03fbe797a3dcf58abe8ff9b7
Lex Albrandt <lex.albrandt@gmail.com>
2025-10-15 10:16:00 -0700

    Add skeleton switch functionality

--- school/CS333/labs/lab1/cae-xor.c
+++ school/CS333/labs/lab1/cae-xor.c
@@ -7,16 +7,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <getopt.h>
 
 // Command line options
 // -e encrypt (argument required)
 // -d decrypt (argument required)
+// -c encryption for caesar only (argument required)
+// -x encryption for xor only (argument required)
 // -h help (no argument required)
 // -D diagnostic (no argument required)
 // UPDATE THESE
-#define OPTIONS "e: d: hD"
+#define OPTIONS "e: d: c: x: hD"
 
 // Noisy debug print
+// -DNOISY_DEBUG in makefile (disable there)
 #ifdef NOISY_DEBUG
 # define NOISY_DEBUG_PRINT fprintf(stderr, "%s %s %d\n", __FILE__, __func__, __LINE__)
 #else 
@@ -26,10 +30,53 @@
 
 int main(int argc, char *argv[])
 {
-
+    NOISY_DEBUG_PRINT;
     // getopt structure
     {
         int opt = 0;                        // getopt option variable
+
+        while ((opt = getopt(argc, argv, OPTIONS)) != -1)
+        {
+            switch(opt)
+            {
+                // encrypt (argument required)
+                case 'e':
+                    NOISY_DEBUG_PRINT;
+                    printf("encrypt string: %s\n", optarg);
+                    break;
+                // decrypt (argument required) 
+                case 'd':
+                    NOISY_DEBUG_PRINT;
+                    printf("decrypt string: %s\n", optarg);
+                    break;
+                // encrypt with caesar cipher only (argument required)
+                case 'c':
+                    NOISY_DEBUG_PRINT;
+                    printf("encrypt string: %s\n", optarg);
+                    break;
+                // encrypt with xor cipher only (argument required)
+                case 'x':
+                    NOISY_DEBUG_PRINT;
+                    printf("encrypt string: %s\n", optarg);
+                    break;
+                // help (no argument required)
+                // UPDATE LATER
+                case 'h':
+                    NOISY_DEBUG_PRINT;
+                    printf("helpful info\n");
+                    break;
+                // Diagnostic info
+                // UPDATE LATER
+                case 'D':
+                    NOISY_DEBUG_PRINT;
+                    printf("Diagnostic info\n");
+                    break;
+                default:
+                    NOISY_DEBUG_PRINT;
+                    break;
+            }
+        }
+        NOISY_DEBUG_PRINT;
 
     }
 
