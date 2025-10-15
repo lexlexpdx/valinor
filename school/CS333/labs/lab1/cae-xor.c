07d5c48cfd9dc5daec090938cbc8b80a9bd245f4
Lex Albrandt <lex.albrandt@gmail.com>
2025-10-15 09:29:54 -0700

    Add noisy debug and options macro

--- school/CS333/labs/lab1/cae-xor.c
+++ school/CS333/labs/lab1/cae-xor.c
@@ -1,11 +1,37 @@
+// Lex Albrandt
+// albrandt@pdx.edu
+// CS333
+// cae-xor source file
+
+
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+
+// Command line options
+// -e encrypt (argument required)
+// -d decrypt (argument required)
+// -h help (no argument required)
+// -D diagnostic (no argument required)
+// UPDATE THESE
+#define OPTIONS "e: d: hD"
+
+// Noisy debug print
+#ifdef NOISY_DEBUG
+# define NOISY_DEBUG_PRINT fprintf(stderr, "%s %s %d\n", __FILE__, __func__, __LINE__)
+#else 
+# define NOISY_DEBUG_PRINT
+#endif // NOISY_DEBUG
 
 
 int main(int argc, char *argv[])
 {
-    printf("Hello world");
 
+    // getopt structure
+    {
+        int opt = 0;                        // getopt option variable
+
+    }
 
     return EXIT_SUCCESS;
 }
