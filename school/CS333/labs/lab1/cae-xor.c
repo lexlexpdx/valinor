8dd318c8702a4c6c8887c9cdaba2344de68ac766
Lex Albrandt <lex.albrandt@gmail.com>
2025-10-19 11:07:37 -0700

    Add make file changes

--- school/CS333/labs/lab1/cae-xor.c
+++ school/CS333/labs/lab1/cae-xor.c
@@ -19,7 +19,7 @@
 // -x encryption for xor only (argument required)
 // -h help (no argument required)
 // -D diagnostic (no argument required)
-#define OPTIONS "e:d:c:x:hD"
+#define OPTIONS "edc:x:hD"
 #define BUFFER_SIZE 5000
 #define PRINT_CHAR_START 32
 #define PRINT_CHAR_END 126
@@ -34,42 +34,44 @@
 #endif // NOISY_DEBUG
 
 // Prototypes
-void caesar_encrypt(char *);
+void caesar_encrypt(char *buffer, size_t plain_text_length, char *key_string);
+void xor_encode(char *buffer, size_t plain_text_length, char *key_string);
 
 int main(int argc, char *argv[])
 {
-    bool enc_dec = false;               // if enc_dec == true: encrypt was last to be seen, else decrypt was last to be seen
-    char *key_string = NULL;           // stores optarg for processing after command line is parsed
+    bool enc_dec = true;                                                    // if enc_dec == true: encrypt was last to be seen (it is also the default value), else decrypt was last to be seen
+    bool diagnostic = false;                                                // Indicates if diagnostic flag is set
+    char *cae_key_string = NULL;                                            // Stores caesar key string after getopt parsing 
+    char *xor_key_string = NULL;                                            // Stores xor key string after getopt parsing 
+    char buffer[BUFFER_SIZE];                                               // Read in buffer
+    size_t plain_text_length;                                               // Length of plain text bytes read
 
     NOISY_DEBUG_PRINT;
 
     // getopt structure
     {
-        int opt = 0;                        // getopt option variable
+        int opt = 0;                                                        // getopt option variable
 
         while ((opt = getopt(argc, argv, OPTIONS)) != -1)
         {
             NOISY_DEBUG_PRINT;
             switch(opt)
             {
-                // encrypt (argument required)
+                // encrypt
                 case 'e':
                     enc_dec = true;
-                    key_string = optarg;
                     break;
-                // decrypt (argument required) 
+                // decrypt
                 case 'd':
                     enc_dec = false;
-                    key_string = optarg;
                     break;
-                // encrypt with caesar cipher only (argument required)
+                // caesar cipher encrypt key
                 case 'c':
-                    key_string = optarg;
-                    caesar_encrypt(key_string);
+                    cae_key_string = optarg;
                     break;
-                // encrypt with xor cipher only (argument required)
+                // xor cipher encrypt key
                 case 'x':
-                    key_string = optarg;
+                    xor_key_string = optarg;
                     break;
                 // help (no argument required)
                 // UPDATE LATER
@@ -77,58 +79,75 @@ int main(int argc, char *argv[])
                     printf("helpful info\n");
                     break;
                 // Diagnostic info
-                // UPDATE LATER
                 case 'D':
+                    diagnostic = true;
                     printf("Diagnostic info\n");
                     break;
                 default:
                     break;
             }
         }
-        NOISY_DEBUG_PRINT;
-        if (enc_dec)
-        {
-            // pass to encryption function
-            printf("Encryption function will be called on %s\n", key_string);
-            caesar_encrypt(key_string);
-        }
-        else
-        {
-            // pass to decryption function
-            printf("Decryption function will be called on %s\n", key_string);
-        }
     }
 
+        NOISY_DEBUG_PRINT;
+        while ((plain_text_length = read(0, buffer, BUFFER_SIZE)) > 0)
+        {
+            if (enc_dec)
+            {
+                if (cae_key_string)
+                {
+                    caesar_encrypt(buffer, plain_text_length, cae_key_string);
+                }
+                if (xor_key_string)
+                {
+                    xor_encode(buffer, plain_text_length, xor_key_string);
+                }
+
+            }
+            else
+            {
+                printf("uffffff\n");
+            }
+            write(1, buffer, plain_text_length);
+        }
 
     return EXIT_SUCCESS;
 }
 
-void caesar_encrypt(char *key_string)
+
+void caesar_encrypt(char *buffer, size_t plain_text_length, char *key_string)
 {
     char character;
-    char buffer[BUFFER_SIZE];
-    ssize_t num_bytes_read = 0;
+    size_t key_index = 0;
     size_t key_length = strlen(key_string);
-    size_t key_index;
     int shift = 0;
 
     NOISY_DEBUG_PRINT;
-    while ((num_bytes_read = read(0, buffer, BUFFER_SIZE)) > 0)
+    for (size_t i = 0; i < plain_text_length; i++)
     {
-        for (ssize_t i = 0; i < num_bytes_read; i++)
+        character = buffer[i];
+        if (isprint(character))                                                                         // only runs if the character is printable
         {
-            character = buffer[i];
-            if (isprint(character))                                                                         // only runs if the character is printable
-            {
-                shift = key_string[key_index % key_length] - PRINT_CHAR_START;                              // Takes key_string @ i value, mods by the key length for wrap around
-                                                                                                            // subtracts the value of ' ' (32) to determine shift
-                character = ((character - PRINT_CHAR_START) + shift) % PRINT_RANGE + PRINT_CHAR_START;      // starts with character value and subtracts 32, adds shift value. 
-                                                                                                            // then that whole value is modded by the print range for wrap around
-                                                                                                            // and start is added back
-                key_index++;
-            }
-            buffer[i] = character;
+            shift = key_string[key_index % key_length] - PRINT_CHAR_START;                              // Takes key_string @ i value, mods by the key length for wrap around
+                                                                                                        // subtracts the value of ' ' (32) to determine shift
+            character = ((character - PRINT_CHAR_START) + shift) % PRINT_RANGE + PRINT_CHAR_START;      // starts with character value and subtracts 32, adds shift value. 
+                                                                                                        // then that whole value is modded by the print range for wrap around
+                                                                                                        // and start is added back
+            key_index++;
         }
-        write(1, buffer, num_bytes_read);
+        buffer[i] = character;
     }
+}
+
+
+void xor_encode(char *buffer, size_t plain_text_length, char *key_string)
+{
+        size_t key_index = 0;
+        size_t key_length = strlen(key_string);
+
+        for (size_t i = 0; i < plain_text_length; i++)
+        {
+            buffer[i] = buffer[i] ^ (key_string[key_index % key_length]);
+            key_index++;
+        }
 }
