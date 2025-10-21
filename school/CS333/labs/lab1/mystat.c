d79b0c6686c738a054df3d25375debb24f9eae11
Lex <albrandt@pdx.edu>
2025-10-21 10:57:04 -0700

    Add mystat changes

--- school/CS333/labs/lab1/mystat.c
+++ school/CS333/labs/lab1/mystat.c
@@ -3,18 +3,77 @@
 // Lab 1
 // mystat source code
 
-#include <stdlib.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/sysmacros.h>
+#include <sys/types.h>
+#include <time.h>
 #include <unistd.h>
-#include <getopt.h>
-#include <stdbool.h>
-#include <ctype.h>
-#include <string.h>
 
-int main()
+#define _GNU_SOURCE
+
+int main(int argc, char *argv[])
 {
-    
-    
+    struct stat sb;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "Usage: %s <pathname\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    if (lstat(argv[1], &sb) == -1)
+    {
+        perror("lstat");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("File type:                  ");
+    switch (sb.st_mode & S_IFMT)
+    {
+        case S_IFBLK:
+        {
+            printf("block device\n");
+            break;
+        }
+        case S_IFCHR:
+        {
+            printf("character device\n");
+            break;
+        }
+        case S_IFDIR:
+        {
+            printf("directory\n");
+            break;
+        }
+        case S_IFIFO:
+        {
+            printf("FIFO/pipe\n");
+            break;
+        }
+        case S_IFLNK:
+        {
+            printf("symlink\n");
+            break;
+        }
+        case S_IFREG:
+        {
+            printf("regular file\n");
+            break;
+        }
+        case S_IFSOCK:
+        {
+            printf("socket\n");
+            break;
+        }
+        default:
+        {
+            printf("unknown?\n");
+            break;
+        }
+    }
 
     return EXIT_SUCCESS;
 }
