2e9352aee089de4a163b854ad03313591ce82451
Lex Albrandt <lex.albrandt@gmail.com>
2025-10-23 18:23:14 -0700

    Add functions to ccwc count_bytes - counts the number of bytes count_lines - counts the number of lines

--- side_projects/coding_challenges/wc/ccwc.c
+++ side_projects/coding_challenges/wc/ccwc.c
@@ -7,14 +7,13 @@
 #define OPTIONS "c:l:w:m:"
 #define BUFFER 50000
 
+// Prototypes
+void count_bytes(char *);
+void count_lines(char *);
+
 
 int main(int argc, char *argv[])
 {
-    FILE *file;
-    char buffer[BUFFER];
-    ssize_t num;
-    size_t total = 0;
-    int fd;
 
     if (argc < 2)
     {
@@ -30,22 +29,74 @@ int main(int argc, char *argv[])
             switch (opt)
             {
                 case 'c':
-                    file = fopen(optarg, "r");
-                    if (file == NULL)
-                    {
-                        perror("Error opening file");
-                        exit(EXIT_FAILURE);
-                    }
-                    fd = fileno(file);
-                    while ((num = read(fd, buffer, BUFFER)) > 0)
-                    {
-                        total += num;
-                    }
-                    printf("%zu %s\n", total, optarg);
+                {
+                    count_bytes(optarg);
+                    break;
+                }
+                case 'l':
+                {
+                    count_lines(optarg);
+                    break;
+                }
                 default:
                     break;
             }
         }
     }
+
+}
+
+
+void count_bytes(char *file_name)
+{
+    FILE *file;
+    char buffer[BUFFER];
+    ssize_t num = 0;
+    size_t total = 0;
+    int fd;
+
+    file = fopen(file_name, "r");
+
+    if (file == NULL)
+    {
+        perror("empty file");
+        exit(EXIT_FAILURE);
+    }
+    fd = fileno(file);
+    while ((num = read(fd, buffer, BUFFER)) > 0)
+    {
+        total += num;
+    }
+
+    fclose(file);
+
+    printf("%zu %s\n", total, optarg);
+
+}
+
+
+void count_lines(char *file_name)
+{
+    FILE *file;
+    size_t total = 0;
+    char c;
+
+    file = fopen(file_name, "r");
+
+    if (file == NULL)
+    {
+        perror("file is empty");
+        exit(EXIT_FAILURE);
+    }
+
+    while ((c = fgetc(file)) != EOF)
+    {
+        if (c == '\n')
+            ++total;
+    }
+
+
+    fclose(file);
+    printf("%zu %s", total, file_name);
 
 }
