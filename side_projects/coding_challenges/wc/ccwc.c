3313ff0291bebb078c2236ba21f7655adea36022
Lex Albrandt <lex.albrandt@gmail.com>
2025-11-06 15:26:07 -0800

    Add new lab2 dir and files

--- side_projects/coding_challenges/wc/ccwc.c
+++ side_projects/coding_challenges/wc/ccwc.c
@@ -3,17 +3,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <ctype.h>
+#include <stdbool.h>
 
 #define OPTIONS "c:l:w:m:"
 #define BUFFER 50000
 
 // Prototypes
 void count_bytes(char *);
-void count_lines(char *);
+void count_lines(char *, bool is_default);
+void count_words(char *, bool is_default);
+void count_chars(char *);
 
 
 int main(int argc, char *argv[])
 {
+
+    bool option_found = false;
+    bool is_default = false;
+    int opt;
 
     if (argc < 2)
     {
@@ -21,29 +29,49 @@ int main(int argc, char *argv[])
         exit(EXIT_FAILURE);
     }
 
-    {
-        int opt = 0;
 
-        while ((opt = getopt(argc, argv, OPTIONS)) != -1)
+    while ((opt = getopt(argc, argv, OPTIONS)) != -1)
+    {
+        option_found = true;
+        switch (opt)
         {
-            switch (opt)
+            case 'c':
             {
-                case 'c':
-                {
-                    count_bytes(optarg);
-                    break;
-                }
-                case 'l':
-                {
-                    count_lines(optarg);
-                    break;
-                }
-                default:
-                    break;
+                count_bytes(optarg);
+                break;
             }
+            case 'l':
+            {
+                count_lines(optarg, is_default);
+                break;
+            }
+            case 'w':
+            {
+                count_words(optarg, is_default);
+                break;
+            }
+            case 'm':
+            {
+                count_chars(optarg);
+                break;
+            }
+            default:
+                break;
         }
     }
 
+    if (!option_found && optind < argc)
+    {
+        for (int i = optind; i < argc; i++)
+        {
+            is_default = true;
+            count_lines(argv[i], is_default);
+            count_words(argv[i], is_default);
+            count_chars(argv[i]);
+        }
+    }
+
+    return EXIT_SUCCESS;
 }
 
 
@@ -75,7 +103,7 @@ void count_bytes(char *file_name)
 }
 
 
-void count_lines(char *file_name)
+void count_lines(char *file_name, bool is_default)
 {
     FILE *file;
     size_t total = 0;
@@ -85,7 +113,7 @@ void count_lines(char *file_name)
 
     if (file == NULL)
     {
-        perror("file is empty");
+        perror("error opening file");
         exit(EXIT_FAILURE);
     }
 
@@ -97,6 +125,73 @@ void count_lines(char *file_name)
 
 
     fclose(file);
-    printf("%zu %s", total, file_name);
+    if (is_default)
+        printf("%zu\t", total);
+    else
+        printf("%zu %s\n", total, file_name);
+
+}
+
+
+void count_words(char *file_name, bool is_default)
+{
+    FILE *file;
+    size_t total = 0;
+    char c;
+    bool in_word;
+
+    file = fopen(file_name, "r");
+
+    if (file == NULL)
+    {
+        perror("error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    while ((c = fgetc(file)) != EOF)
+    {
+        if (isspace(c))
+        {
+            in_word = false;
+        }
+        else if (!in_word)
+        {
+            in_word = true;
+            ++total;
+        }
+    }
+
+    fclose(file);
+    if (is_default)
+        printf("%zu\t", total);
+    else
+        printf("%zu %s\n", total, file_name);
+
+}
+
+
+void count_chars(char *file_name)
+{
+    FILE *file;
+    size_t total = 0;
+    char c;
+
+    file = fopen(file_name, "r");
+
+    if (file == NULL)
+    {
+        perror("error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    while ((c = fgetc(file)) != EOF)
+    {
+        ++total;
+    }
+
+    fclose(file);
+
+    printf("%zu %s\n", total, file_name);
+
 
 }
