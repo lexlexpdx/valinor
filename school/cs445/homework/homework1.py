a94f84f30b6e7d428571d3ffece6565e6adab843
Lex Albrandt <lex.albrandt@gmail.com>
2026-01-14 19:02:35 -0800

    Begin work on HW1

---
+++ school/cs445/homework/homework1.py
@@ -0,0 +1,17 @@
+# %% [markdown]
+# Lex Albrandt  
+# CS445  
+# HW 1
+
+# %% [markdown]
+# Question 1:  
+#  
+# 1a)<br>
+# A target variable the retailer could use would be the future sales of a product
+# <br><br>
+# 1b)<br>
+# For this linear regression the numeric score would be a feature as well as each of the  
+# judgement words (we would tokenize these). So the feature vector would consist of the numeric score  
+# and all other tokens.  
+# The model could be written as:  
+# $$y = \beta_0 + \beta_1 x_1 + \beta_2 x_2 + ... + \beta_n x_n$$
