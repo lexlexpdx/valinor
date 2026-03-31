124c1980565491f55521110991e4c567584954b6
Lex Albrandt <lex.albrandt@gmail.com>
2026-03-31 11:39:59 -0700

    Add new classes and move old

---
+++ school/SYSC410/Labs/Lab1/lab1_orange.py
@@ -0,0 +1,23 @@
+# %% [markdown]
+# Lex Albrandt  
+# SYSC 410  
+# Lab 1  
+#   
+
+# %% [markdown]
+# ## Data Import and Data Table  
+#   
+# Data was loaded with CSV load and a data table was generated with the following results:  
+#   
+# There are 344 rows with approximately 0.8% missing data.  
+#   
+# ![Penguins Data Table](./screenshots/data_table.png)
+#
+
+# %% [markdown]
+# ## Scatter Plot  
+#   
+# A scatter plot was added showing clear cluster separation (with a small amount of overlap) between 
+# the Adelie, Chinstrap, and Gentoo species.  
+#   
+# ![Penguins Scatter Plot](./screenshots/scatter_plot.png)
